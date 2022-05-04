24b838a132ea2a09171b719cca3fac9c32caddf1
LordMuda ID <91729945+LordMudaID@users.noreply.github.com>
2022-05-04 14:56:20 +0700

    Update alive.py

--- Music/Plugins/alive.py
+++ Music/Plugins/alive.py
@@ -42,7 +42,7 @@ async def _human_time_duration(seconds):
     return ', '.join(parts)
 
 
-KYY_IMG = "https://telegra.ph/file/2b610c918dec590d2777b.jpg"
+KYY_IMG = "https://te.legra.ph/file/106eda83a7a549a6eeb24.jpg"
 
 
 @app.on_message(filters.command(["alive", "alive@Tg_Vc_00_Bot"]))
